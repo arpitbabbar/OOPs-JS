b267e81dcc5fbcf6e3b3efaa3b90d14811d57970
Arpit Babbar <arpitbabbar01@gmail.com>
2023-01-20 19:24:15 +0530

    oops pdf + prototypes

--- classes.js
+++ classes.js
@@ -5,10 +5,21 @@ class Product {
     }
 
     display(){
-        console.log("Name - " + this.name + " Price - " + this.price);
+        console.log("Name - " + this.name + " Price - " + this.price + " Discount - " + this.discount);
+    }
+    setDiscount(){
+        this.discount = 10;
     }
 }
 
 const p1 = new Product("iphone", 100000);
 console.log(p1);
 p1.display();
+p1.setDiscount();
+
+const p2 = new Product("mac", 13456);
+p2.setDiscount();
+console.log(p2);
+
+p2.display();
+
